05ca2b7715e692b7293fb70c6ea3a8b0069ab18c
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2021-11-12 16:27:07 +0000

    Add files via upload

---
+++ Clock/Runner.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+public class Runner
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		JFrame window = new JFrame();
+		ClockPannel clock = new ClockPannel();
+		window.setContentPane(clock);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setTitle("Clock");
+		window.setSize(400,300);
+		window.setVisible(true);
+		window.setBackground(Color.black);
+	
+		
+	}
+	
+
+}
+ 
