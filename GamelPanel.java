7bfca0229a556a546db47c738be2d003247f046e
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2021-11-17 14:26:41 +0000

    Add files via upload

--- GamelPanel.java
+++ GamelPanel.java
@@ -37,4 +37,3 @@ public class GamelPanel extends JPanel
 	}
 	
 }
-
--- Interface.java
+++ Interface.java
@@ -1,16 +1,39 @@
-import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
 public class Interface extends JPanel
 {
-	private GamelPanel myGamePanel = new GamelPanel()
+	private Tile[] myTiles = new Tile[9];
+	public char myReturn = 'X';
 	
-	public Interface() 
+	public Interface()
+		
 	{
-		this.setLayout(new BorderLayout());
-		this.add(myGamePanel, BorderLayout.CENTER);
+		this.setLayout(new GridLayout (3,3));
+		for(int i=0; i<myTiles.length; i++)
+		{
+			myTiles[i] = new Tile();
+			this.add(myTiles[i]);
+		}
 	}
-			
+	
+	public void reset()
+	{
+		for(int i=0; i<myTiles.length; i++)
+		{
+			myTiles[i].clear();
+		}
+	}
+	
+	public char getReturn()
+	{
+		return myReturn;
+	}
+	
+	public char checkWinner()
+	{
+		return ' ';
+	}
 	
 }
