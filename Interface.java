69fbb1f869cb036485c9e0ad2e6caf66e3d34106
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2021-11-18 21:47:04 +0000

    Add files via upload

--- Interface.java
+++ Interface.java
@@ -4,36 +4,36 @@ import javax.swing.JPanel;
 
 public class Interface extends JPanel
 {
-	private Tile[] myTiles = new Tile[9];
-	public char myReturn = 'X';
-	
-	public Interface()
-		
-	{
-		this.setLayout(new GridLayout (3,3));
-		for(int i=0; i<myTiles.length; i++)
-		{
-			myTiles[i] = new Tile();
-			this.add(myTiles[i]);
-		}
-	}
-	
-	public void reset()
-	{
-		for(int i=0; i<myTiles.length; i++)
-		{
-			myTiles[i].clear();
-		}
-	}
-	
-	public char getReturn()
-	{
-		return myReturn;
-	}
-	
-	public char checkWinner()
-	{
-		return ' ';
-	}
-	
+    private Tile[] myTiles = new Tile[9];
+    public char myReturn = 'X';
+
+    public Interface()
+
+    {
+        this.setLayout(new GridLayout (3,3));
+        for(int i=0; i<myTiles.length; i++)
+        {
+            myTiles[i] = new Tile();
+            this.add(myTiles[i]);
+        }
+    }
+
+    public void reset()
+    {
+        for(int i=0; i<myTiles.length; i++)
+        {
+            myTiles[i].clear();
+        }
+    }
+
+    public char getReturn()
+    {
+        return myReturn;
+    }
+
+    public char checkWinner()
+    {
+        return ' ';
+    }
+
 }
--- Tile.java
+++ Tile.java
@@ -1,27 +1,27 @@
-import javax.swing.JButton;
+import javax.swing.*;
 
 public class Tile extends JButton
 {
-	private char mySymbol;
-	private String setText;
-	
-	public void setSymbol(char s)
-	{
-		mySymbol=s;
-		this.setText =(""+s);
-	}
-	
-	public char getSymbol()
-	{
-		return mySymbol;
-	}
-	
-	public void clear()
-	{
-		mySymbol = ' ';
-		this.setText("");
-	}
-	
-	
-	
+    private char mySymbol;
+    private String setText;
+
+    public void setSymbol(char s)
+    {
+        mySymbol=s;
+        this.setText =(""+s);
+    }
+
+    public char getSymbol()
+    {
+        return mySymbol;
+    }
+
+    public void clear()
+    {
+        mySymbol = ' ';
+        this.setText("");
+    }
+
+
+
 }
