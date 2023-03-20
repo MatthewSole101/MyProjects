282790ab44b19e12ef82ecec8ab4ac9cdd1d8629
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2023-03-20 07:48:36 +0000

    Update Simulation.java

--- NEA NEW/src/Simulation.java
+++ NEA NEW/src/Simulation.java
@@ -1,42 +1,42 @@
 public class Simulation
+{
+    Atom[] atoms = new Atom[100];
+
+
+    public Simulation()
     {
-        Atom[] atoms = new Atom[100];
-
-
-        public Simulation()
+        for (int i=0; i<atoms.length; i++)
         {
-            for (int i=0; i<atoms.length; i++)
-            {
-                atoms[i] =  new Atom(0.1);
-            }
+            atoms[i] =  new Atom(0.1);
         }
+    }
 
-        public int countUndecayedAtoms()
+    public int countUndecayedAtoms()
+    {
+        int count = 0;
+        for(Atom a: atoms)
         {
-            int count = 0;
-            for(Atom a: atoms)
-            {
-                if(a.getDecayed() == false)
-                    count++;
-            }
-            return count;
+            if(a.getDecayed() == false)
+                count++;
         }
+        return count;
+    }
 
-        public void tick()
+    public void tick()
+    {
+        for(Atom a: atoms)
         {
-            for(Atom a: atoms)
-            {
-                a.decay();
-            }
+            a.decay();
         }
+    }
 
-        public static void main(String[] args)
+    public static void main(String[] args)
+    {
+        Simulation bill = new Simulation();
+        for(int i=0; i<20; i++)
         {
-            Simulation bill = new Simulation();
-            for(int i=0; i<20; i++)
-            {
-                bill.tick();
-                System.out.println(bill.countUndecayedAtoms()+" atoms undecayed");
-            }
+            bill.tick();
+            System.out.println(bill.countUndecayedAtoms()+" atoms undecayed");
         }
+    }
 }
