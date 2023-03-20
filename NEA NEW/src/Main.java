aca11d0282eade863ba362136fa9de36d46ccaeb
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2023-03-20 07:48:06 +0000

    Update Main.java

--- NEA NEW/src/Main.java
+++ NEA NEW/src/Main.java
@@ -1,34 +1,41 @@
-//import javax.swing.*;
-//import java.awt.*;
-//
-//public class Main extends JPanel
-//{
-//    private Grid[][] myGrid = new Grid [5][5];
-//    public Main()
-////    {
-////        JPanel buttonPanel = new JPanel();
-////        this.setLayout(new GridLayout(5, 5 ));
-////        for (int y = 0; y < myGrid.length; y++)
-////        {
-////            myGrid.add (new Grid());
-////            myG
-////
-////            myBall.get(i).myxpos((int) Math.random()*100);
-////            for (int x = 0; x < myGrid[y].length; x++)
-////            {
-////                myGrid[y][x] = new Grid(x, y);
-////
-////            }
-////        }
-//    }
-//
-////    public void paintComponent (Graphics g) {
-////        super.paintComponent(g);
-////        Graphics2D g2d = (Graphics2D) g;
-//////        g2d.setColor(myBall.get(i).getmyColor());
-//////        g2d.fillOval(myBall.get(i).getmyxpos(), myBall.get(i).getmyypos(), myBall.get(i).getmySize(), myBall.get(i).getmySize());
-//////        g2d.setColor(Color.WHITE);
-////
-////
-////    }
-//}
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JPanel
+{
+    private Grid[][] myGrid = new Grid [5][5];
+    public Main()
+    {
+        JPanel buttonPanel = new JPanel();
+        this.setLayout(new GridLayout(5, 5 ));
+        for (int y = 0; y < myGrid[x].length; y++)
+        {
+//            myGrid.add (new Grid());
+//            myG
+
+//            myBall.get(i).myxpos((int) Math.random()*100);
+            for (int x = 0; x < myGrid[y].length; x++)
+            {
+                myGrid[y][x] = new Grid(x, y);
+                this.add(myGrid[y][x]);
+
+            }
+        }
+    }
+
+    public void paintComponent (Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        for (int x=0; x<myGrid[y].length; x++)
+        {
+            for (int y=0; x<myGrid[y].length; y++)
+                g2d.fillOval(myGrid[i][i], );
+        }
+
+//            g2d.setColor(myBall.get(i).getmyColor());
+//            g2d.fillOval(myBall.get(i).getmyxpos(), myBall.get(i).getmyypos(), myBall.get(i).getmySize(), myBall.get(i).getmySize());
+//            g2d.setColor(Color.WHITE);
+
+
+    }
