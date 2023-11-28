ff04fd5ca5113e26f537f9af40c958b2101c9265
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2023-11-28 17:12:18 +0000

    Update task1.py

--- pythonProjectassessment2/task1.py
+++ pythonProjectassessment2/task1.py
@@ -43,38 +43,14 @@ while count != count2:
 
 
 
-else:
-    print('The sum is', total)
 
-# if list[count3] > 1:
-#
-#     for count3 in range(2, int(list[count3]/2)+1):
-#         if (list[count3] % count3) == 0:
-#             print(list[count3], "is NOT a prime number")
-#     else:
-#         print(list[count3], "is a prime number")
-#         primelist.append(list[count3])
-# else:
-#     print(list[count3], 'is not a prime number')
 
-# for count3 in range(2, int(list[count3] / 2) + 1):
-#     if list[count3] <= 1:
-#         print(list[count3], 'is not a prime number')
-#     if list[count3] == 2:
-#         print(list[count3], 'is a prime number')
-#         primelist.append(list[count3])
-#     if list[count3] % count3 == 0:
-#         print(list[count3], 'is not a prime number')
-#     else:
-#         print(list[count3], 'is not a prime number')
-#         primelist.append(list[count3])
 
 while length != count3:
     prime = list[count3]
     checkPrime(prime)
     count3 = count3 + 1
-else:
-    print('The prime numbers are', primelist)
+
 
 
 
@@ -84,5 +60,5 @@ else:
 lengthprime = len(primelist)
 mean = total/count
 format(mean, '.2')
-print('The minimum number is', list[0], 'and the maximum is', list[-1], 'and the mean is', mean, 'and the sum is', total)
+print('The sum is', total, 'and the minimum number is', list[0], 'and the maximum is', list[-1], 'and the mean is', mean, 'and the sum is', total)
 print('There are', lengthprime, 'prime numbers:', primelist)
