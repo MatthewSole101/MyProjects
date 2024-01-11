548655a42aa69961e813d1bb58d8dc517196f424
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2024-01-11 02:03:43 +0000

    Update task1.py

--- pythonProjectassessment2/task1.py
+++ pythonProjectassessment2/task1.py
@@ -1,15 +1,14 @@
 
-def checkPrime(num):
-    if num == 2:
-        print(num, 'is a prime number')
-        primelist.append(num)
-    for i in range(2, num):
-        if num % i == 0 or num == 9:
-            break
-        else:
-            print(num, 'is a prime number')
-            primelist.append(num)
-            break
+def checkPrime(x):
+    #If the number is greater than 1 and is not divisible by the num - it is prime
+    if x > 1:
+        for num in range(2, x):
+            if x % num == 0:
+                return False
+        return True
+    return False
+
+
 
 list = []
 primelist = []
@@ -21,30 +20,50 @@ count3 = 0
 length = 0
 mean = 0
 isPrime = False
-inp = int(input("Write a integer: "))
-list.append(inp)
-
-while inp != 00:
-    inp = int(input("Write another integer or stop by typing '0': "))
+try:
+    inp = int(input("Write a integer: "))
     list.append(inp)
-    count = count + 1
 
-else:
-    list.pop(-1)
-    length = len(list)
-    list.sort()
+    #Repeatedly asking the user for numbers until they say to stop(typing 0)
+    while inp != 00:
+        inp = int(input("Write another integer or stop by typing '0': "))
+        list.append(inp)
+        #count is going to be used later to figure out the mean
+        count = count + 1
 
-while count != count2:
-    total = total + list[count2]
-    count2 = count2 + 1
+    else:
+        #Sorting the list and removing 0 at the end which the user had to put to stop entering numbers
+        list.pop(-1)
+        length = len(list)
+        list.sort()
 
-while length != count3:
-    prime = list[count3]
-    checkPrime(prime)
-    count3 = count3 + 1
+    while count != count2:
+        #Adding up every number and saving it into the variable total
+        total = total + list[count2]
+        count2 = count2 + 1
+
+    while length != count3:
+        #Checks the prime number of every number the user inputted
+        if checkPrime(list[count3]):
+            primelist.append(list[count3])
+
+        count3 = count3 + 1
+#Error Handling
+except ValueError:
+    print("Wrong value inputted")
+    exit()
+except TypeError:
+    print("Invalid input")
+except ZeroDivisionError:
+    print("Invalid input")
+
+
+
+
 
 lengthprime = len(primelist)
+#Figuring out the mean of the numbers
 mean = total/count
 format(mean, '.2')
-print('The sum is', total, 'and the minimum number is', list[0], 'and the maximum is', list[-1], 'and the mean is', mean, 'and the sum is', total)
+print('The sum is', total, 'and the minimum number is', list[0], 'and the maximum is', list[-1], 'and the mean is', mean)
 print('There are', lengthprime, 'prime numbers:', primelist)
